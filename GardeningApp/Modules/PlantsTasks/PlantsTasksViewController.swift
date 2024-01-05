2617748bec86a0cb04f89c7fc4da5f717e40a484
kistalex <kistalexander@yandex.ru>
2024-01-05 20:50:53 +0400

    Реализован DatePicker и добавление/отображение задач пользователя по нажатию на кнопку в PlantTask модуле. Установлен цвет фона у navigationController.

--- GardeningApp/Modules/PlantsTasks/PlantsTasksViewController.swift
+++ GardeningApp/Modules/PlantsTasks/PlantsTasksViewController.swift
@@ -7,9 +7,11 @@
 
 import UIKit
 import SwiftUI
+import SnapKit
 
 protocol PlantsTasksViewProtocol: AnyObject {
     func setDates(dates: [Date])
+    func setTasks(tasks: [Int])
     func setCurrentDate(date: Date)
     func setCurrentDateLabel(date: String)
     func refreshCollectionViewAndScrollToCurrentDate()
@@ -19,12 +21,22 @@ class PlantsTasksViewController: UIViewController {
     // MARK: - Public
     var presenter: PlantsTasksPresenterProtocol?
 
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+
     private let dateLabel = CustomLabel(fontName: UIFont.body(), textColor: .accentLight)
     private let todayButton = CustomTextButton(text: "Today", bgColor: .accentDark)
+
     private lazy var datePickerCollectionView = UICollectionView()
-    private let addTaskButton = CustomImageButton(imageName: "plus.circle", configImagePointSize: 35)
+    private lazy var userTaskCollectionView = UICollectionView()
+
+    private let addTaskButton = CustomImageButton(imageName: "plus.circle", configImagePointSize: 40)
+
     private var dates = [Date]()
+    private var tasks = [Int]()
     private var selectedDate: Date?
+
+    private var userTaskCollectionViewHeight: Constraint?
 
 
     // MARK: - View lifecycle
@@ -32,6 +44,7 @@ class PlantsTasksViewController: UIViewController {
         super.viewDidLoad()
         initialize()
         presenter?.viewDidLoad()
+
     }
 
     override func viewDidLayoutSubviews() {
@@ -47,26 +60,70 @@ private extension PlantsTasksViewController {
         title = "Plants Tasks"
         setupViews()
     }
+    
 
     private func setupCollectionViewAppearance(){
-        datePickerCollectionView.layer.cornerRadius = datePickerCollectionView.frame.size.height / 12
-        datePickerCollectionView.layer.masksToBounds = true
-        datePickerCollectionView.layer.borderWidth = 1
-        datePickerCollectionView.layer.borderColor = UIColor.accentLight.cgColor
+        setCollectionLayer(collectionView: datePickerCollectionView, cornerRadiusDivider: 12, borderWidth: 1, borderColor: UIColor.accentLight.cgColor)
     }
 
-    private func setupViews() {
-        datePickerCollectionView = createCollectionView()
+    private func setupDatePicker(){
+        let insets = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+        datePickerCollectionView = createCollectionView(direction: .horizontal, isPagingEnabled: true, bgColor: .accentLight, insets: insets, lineSpacing: 0, itemSpacing: 2)
         datePickerCollectionView.dataSource = self
         datePickerCollectionView.delegate = self
         datePickerCollectionView.register(CalendarCell.self, forCellWithReuseIdentifier: "\(CalendarCell.self)")
+        datePickerCollectionView.tag = 0
+    }
 
-        [dateLabel, todayButton, addTaskButton, datePickerCollectionView].forEach { item in
-            view.addSubview(item)
+    private func setupUserTasks(){
+        let insets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        userTaskCollectionView = createCollectionView(direction: .vertical, isPagingEnabled: false, bgColor: .accentDark, insets: insets, lineSpacing: 10, itemSpacing: 0)
+        userTaskCollectionView.dataSource = self
+        userTaskCollectionView.delegate = self
+        userTaskCollectionView.isScrollEnabled = false
+        userTaskCollectionView.register(TaskCell.self, forCellWithReuseIdentifier: "\(TaskCell.self)")
+        userTaskCollectionView.tag = 1
+    }
+
+    private func setCollectionLayer(collectionView: UICollectionView, cornerRadiusDivider: CGFloat?, borderWidth: CGFloat, borderColor: CGColor){
+        if let cornerRadiusDivider = cornerRadiusDivider {
+            collectionView.layer.cornerRadius = collectionView.frame.size.height / cornerRadiusDivider
+            collectionView.layer.masksToBounds = true
+            collectionView.layer.borderWidth = borderWidth
+            collectionView.layer.borderColor =  borderColor
+        } else {
+            collectionView.layer.cornerRadius = 0
+        }
+    }
+
+    private func setupViews() {
+
+        setupDatePicker()
+        setupUserTasks()
+        scrollView.showsVerticalScrollIndicator = false
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+
+        addTaskButton.backgroundColor = .red
+
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+
+        contentView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.equalTo(scrollView)
+            make.height.equalTo(scrollView).priority(.low)
+//            make.height.greaterThanOrEqualToSuperview().priority(.required)
+        }
+
+        [dateLabel, todayButton, addTaskButton, datePickerCollectionView, userTaskCollectionView].forEach { item in
+            contentView.addSubview(item)
         }
 
         dateLabel.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.top.equalTo(contentView).offset(20)
             make.centerX.equalToSuperview().inset(20)
         }
 
@@ -79,7 +136,7 @@ private extension PlantsTasksViewController {
         datePickerCollectionView.snp.makeConstraints { make in
             make.top.equalTo(dateLabel.snp.bottom).offset(20)
             make.leading.trailing.equalToSuperview().inset(10)
-            make.height.equalTo(150)
+            make.height.equalToSuperview().dividedBy(5.5)
         }
 
         addTaskButton.snp.makeConstraints { make in
@@ -87,23 +144,34 @@ private extension PlantsTasksViewController {
             make.centerX.equalToSuperview()
         }
 
+        userTaskCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(addTaskButton.snp.bottom).offset(10)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(contentView.snp.bottom)
+            make.height.greaterThanOrEqualTo(400)
+        }
+
         todayButton.addTarget(self, action: #selector(todayButtonTapped), for: .touchUpInside)
+        addTaskButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
 
     }
 
-    private func createCollectionView() -> UICollectionView {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCollectionLayout())
-        collectionView.backgroundColor = .accentLight
-        collectionView.isPagingEnabled = true
+
+
+
+    private func createCollectionView(direction: UICollectionView.ScrollDirection, isPagingEnabled: Bool, bgColor: UIColor, insets: UIEdgeInsets, lineSpacing: CGFloat, itemSpacing: CGFloat) -> UICollectionView {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCollectionLayout(direction: direction, insets: insets, lineSpacing: lineSpacing, itemSpacing:  itemSpacing))
+        collectionView.backgroundColor = bgColor
+        collectionView.isPagingEnabled = isPagingEnabled
         return collectionView
     }
 
-    private func createCollectionLayout() -> UICollectionViewFlowLayout {
+    private func createCollectionLayout(direction: UICollectionView.ScrollDirection, insets: UIEdgeInsets, lineSpacing: CGFloat, itemSpacing: CGFloat) -> UICollectionViewFlowLayout {
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
-        layout.sectionInset = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
-        layout.minimumLineSpacing = 0
-        layout.minimumInteritemSpacing = 2
+        layout.scrollDirection = direction
+        layout.sectionInset = insets
+        layout.minimumLineSpacing = lineSpacing
+        layout.minimumInteritemSpacing = itemSpacing
         return layout
     }
 
@@ -117,22 +185,44 @@ private extension PlantsTasksViewController {
     @objc private func todayButtonTapped(){
         presenter?.todayButtonTapped()
     }
+
+    @objc private func addButtonTapped(){
+        presenter?.addTaskButtonTapped()
+    }
 }
 
 extension PlantsTasksViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return dates.count
+        switch collectionView.tag {
+        case 0:
+            return dates.count
+        case 1:
+            return tasks.count
+        default:
+            return 0
+        }
     }
 
+
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CalendarCell.self)", for: indexPath) as? CalendarCell else {
+        switch collectionView.tag {
+        case 0:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(CalendarCell.self)", for: indexPath) as? CalendarCell else {
+                return UICollectionViewCell()
+            }
+
+            let date = dates[indexPath.item]
+            let isSelected = Calendar.current.isDate(date, inSameDayAs: selectedDate ?? Date())
+            cell.configure(with: date, isSelected: isSelected)
+            return cell
+        case 1:
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(TaskCell.self)", for: indexPath) as? TaskCell else {
+                return UICollectionViewCell()
+            }
+            return cell
+        default:
             return UICollectionViewCell()
         }
-
-        let date = dates[indexPath.item]
-        let isSelected = Calendar.current.isDate(date, inSameDayAs: selectedDate ?? Date())
-        cell.configure(with: date, isSelected: isSelected)
-        return cell
     }
 }
 
@@ -141,18 +231,27 @@ extension PlantsTasksViewController: UICollectionViewDelegateFlowLayout {
         guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
             return CGSize.zero
         }
+        switch collectionView.tag {
+        case 0:
+            let leftInset = flowLayout.sectionInset.left
+            let rightInset = flowLayout.sectionInset.right
+            let totalInset = leftInset + rightInset
 
-        let leftInset = flowLayout.sectionInset.left
-        let rightInset = flowLayout.sectionInset.right
-        let totalInset = leftInset + rightInset
+            let availableWidth = collectionView.bounds.width - totalInset
+            let widthPerItem = availableWidth / 7
+            let height = collectionView.bounds.height - (flowLayout.sectionInset.top + flowLayout.sectionInset.bottom)
 
-        let availableWidth = collectionView.bounds.width - totalInset
-        let widthPerItem = availableWidth / 7
-        let height = collectionView.bounds.height - (flowLayout.sectionInset.top + flowLayout.sectionInset.bottom)
-
-        return CGSize(width: widthPerItem, height: height)
+            return CGSize(width: widthPerItem, height: height)
+        case 1:
+            let height: CGFloat  = 180
+            let width = collectionView.bounds.width - (flowLayout.sectionInset.left + flowLayout.sectionInset.right)
+            return CGSize(width: width, height: height)
+        default:
+            return CGSize.zero
+        }
     }
 }
+
 
 
 extension PlantsTasksViewController: UICollectionViewDelegate {
@@ -185,6 +284,11 @@ extension PlantsTasksViewController: PlantsTasksViewProtocol {
         datePickerCollectionView.reloadData()
     }
 
+    func setTasks(tasks: [Int]) {
+        self.tasks = tasks
+        userTaskCollectionView.reloadData()
+    }
+
     func setCurrentDate(date: Date) {
         selectedDate = date
     }
@@ -198,3 +302,9 @@ extension PlantsTasksViewController: PlantsTasksViewProtocol {
         scrollToSelectedDate(animated: true)
     }
 }
+
+
+
+
+
+/*userTaskCollectionView*/
